62e07586e3b89aafea1ad57c264c92095a9fcab7
aso <titechei@gmail.com>
2020-04-02 00:05:10 +0900

    5 Object Detection

---
+++ py/background_subtractor.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('mp4/vtest_short.mp4')
+
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+while(1):
+    ret, frame = cap.read()
+    if ret:
+    	fgmask = fgbg.apply(frame)
+
+    	cv2.imshow('frame',fgmask)
+    	k = cv2.waitKey(30) & 0xff
+    	if k == 27:
+        	break
+    else:
+    	break
+
+cap.release()
+cv2.destroyAllWindows()
